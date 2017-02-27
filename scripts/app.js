54b828e8c9c5f940f63e484bd4973c358bab1709
NolanHughes <Nhughes987@gmail.com>
2017-02-27 15:22:21 -0600

    Work on sidebar styling and other css

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,24 @@
 $(document).ready(function() {
   var $menu = $("#sidebar-wrapper");
 
-  $(document)
-    .on("click", ".js-menu-open", function() {
+  $(document).on("click", ".js-menu-open", function() {
       $menu.addClass("open");
 
       return false;
     })
-    // Sidebar : Close when Sidebar link clicked
+    // Sidebar : Close when x button is clicked
     .on("click", "#close-btn", function() {
       $menu.removeClass("open");
     })
-    // Sidebar : Close when Sidebar link clicked
+    // Sidebar : Close when sub-menu item clicked
     .on("click", "#list-close-btn", function() {
       $menu.removeClass("open");
     })
-    // Sidebar : Close from outside the Sidebar
+    // Sidebar : Close from outside of the Sidebar
     .on("mouseup", "#main-wrapper", function() {
       $menu.removeClass("open");
     });
-
+    // Sidebar: Close accordion when clicked while open
     $('input[type="checkbox"]').on('change', function() {
    $('input[type="checkbox"]').not(this).prop('checked', false);
     });
