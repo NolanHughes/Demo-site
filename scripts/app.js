41fa47207c2b703232109f2a2b51c5c30459ce96
NolanHughes <Nhughes987@gmail.com>
2018-09-06 14:41:40 -0500

    Update project title to be a link to the project.

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
-  var $menu = $("#sidebar-wrapper");
+  var menu = $("#sidebar-wrapper");
 
   $(document).on("click", ".js-menu-open", function() {
-      $menu.addClass("open");
+      menu.addClass("open");
       return false;
     })
     // Sidebar : Close when x button is clicked
     .on("click", "#close-btn", function() {
-      $menu.removeClass("open");
+      menu.removeClass("open");
     })
     // Sidebar : Close when sub-menu item clicked
     .on("click", "#list-close-btn", function() {
-      $menu.removeClass("open");
+      menu.removeClass("open");
     })
     // Sidebar : Close from outside of the Sidebar
     .on("mouseup", "#main-wrapper", function() {
-      $menu.removeClass("open");
+      menu.removeClass("open");
     });
     // Sidebar: Close accordion when clicked while open
     $('input[type="checkbox"]').on('change', function() {
-   $('input[type="checkbox"]').not(this).prop('checked', false);
+      $('input[type="checkbox"]').not(this).prop('checked', false);
     });
   });
